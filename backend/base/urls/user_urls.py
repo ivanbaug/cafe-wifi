53e07de09c126ca36b1cbb2ce2e6d18639cb2f68
ivanbaug <ivsitioinfo@gmail.com>
2022-02-08 21:19:46 -0500

    use email as username for login

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -4,7 +4,7 @@ from base.views import user_views as views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("token/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    # path("token/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", views.get_users, name="users"),
     path("login/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
